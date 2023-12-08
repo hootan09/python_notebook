9847b24254d241802396d977f39bd423c7fe2db8
niki <hootan09@gmail.com>
2023-12-08 17:53:29 +0330

    add nodejs samples

---
+++ js samples/node onnx-transformerjs/app.js
@@ -0,0 +1,76 @@
+
+const http = require('http');
+const querystring = require('querystring');
+const url = require('url');
+
+const tokenizer_config = require('./cache/Xenova/distilbert-base-uncased-finetuned-sst-2-english/tokenizer_config.json')
+const tokenizerJson = require('./cache/Xenova/distilbert-base-uncased-finetuned-sst-2-english/tokenizer.json');
+
+
+
+class MyClassificationPipeline {
+  static task = 'text-classification';
+  static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
+  static instance = null;
+
+  static async getInstance(progress_callback = null) {
+    if (this.instance === null) {
+      // Dynamically import the Transformers.js library
+      let { pipeline, env, DistilBertTokenizer } = await import('@xenova/transformers');
+
+
+      // NOTE: Uncomment this to change the cache directory
+      env.cacheDir = './cache';
+
+      this.instance = pipeline(this.task, this.model, { progress_callback });
+
+      // let tokenizer = new DistilBertTokenizer(tokenizerJson, tokenizer_config);
+      // let enc = tokenizer.encode('hello world')
+      // let dec = tokenizer.decode(enc)
+      // console.log(enc,dec);
+
+    }
+
+    return this.instance;
+  }
+}
+
+// Comment out this line if you don't want to start loading the model as soon as the server starts.
+// If commented out, the model will be loaded when the first request is received (i.e,. lazily).
+MyClassificationPipeline.getInstance();
+
+// Define the HTTP server
+const server = http.createServer();
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Listen for requests made to the server
+server.on('request', async (req, res) => {
+  // Parse the request URL
+  const parsedUrl = url.parse(req.url);
+
+  // Extract the query parameters
+  const { text } = querystring.parse(parsedUrl.query);
+
+  // Set the response headers
+  res.setHeader('Content-Type', 'application/json');
+
+  let response;
+  if (parsedUrl.pathname === '/classify' && text) {
+    const classifier = await MyClassificationPipeline.getInstance();
+    response = await classifier(text);
+    res.statusCode = 200;
+  } else {
+    response = { 'error': 'Bad request' }
+    res.statusCode = 400;
+  }
+
+  // Send the JSON response
+  res.end(JSON.stringify(response));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+//http://localhost:3000/classify?text=hello donya
